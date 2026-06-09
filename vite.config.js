1d6d3fcbf0c7f0b065e0778c3c9c8999be9d477d
Mayank Sharma <workwithmayanksharma@gmail.com>
2026-06-10 01:10:38 +0530

    feat: update package.json for deployment scripts and add gh-pages dependency; configure BrowserRouter basename in main.jsx; set base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/happy-anniversary/',
   server: {
     allowedHosts: ['pricily-geodynamical-avianna.ngrok-free.dev'],
   },
